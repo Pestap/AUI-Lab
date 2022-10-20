909906c05e9344e17d3fe0bb0515ce22f3122a43
Pestap <90579181+Pestap@users.noreply.github.com>
2022-10-20 23:54:04 +0200

    Adding licences to pilots REST

--- src/main/java/eti/pg/lab/license/controller/PilotLicenseController.java
+++ src/main/java/eti/pg/lab/license/controller/PilotLicenseController.java
@@ -1,18 +1,20 @@
 package eti.pg.lab.license.controller;
 
 
+import eti.pg.lab.license.dto.CreateLicenseRequest;
 import eti.pg.lab.license.dto.GetLicenseResponse;
 import eti.pg.lab.license.dto.GetLicensesResponse;
+import eti.pg.lab.license.dto.UpdateLicenseRequest;
+import eti.pg.lab.license.entity.License;
 import eti.pg.lab.license.service.LicenseService;
 import eti.pg.lab.pilot.controller.PilotController;
 import eti.pg.lab.pilot.entity.Pilot;
 import eti.pg.lab.pilot.service.PilotService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
 
@@ -45,4 +47,37 @@ public class PilotLicenseController
                 .map(value -> ResponseEntity.ok(GetLicenseResponse.entityToDtoMapper().apply(value)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
+    @PutMapping
+    public ResponseEntity<Void> createLicense(@PathVariable("pilotId") int pilotId,
+                                              @RequestBody CreateLicenseRequest request,
+                                              UriComponentsBuilder builder){
+        request.setPilotId(pilotId);
+        Optional<Pilot> pilot = pilotService.find(pilotId);
+        if(pilot.isPresent()){
+            License licenseToAdd = CreateLicenseRequest
+                    .dtoToEntityMapper(id -> pilotService.find(id).orElseThrow())
+                    .apply(request);
+            licenseToAdd = licenseService.create(licenseToAdd);
+            return ResponseEntity.created(builder.pathSegment("api", "pilots", "{pilotId}", "licenses", "{licenseId}")
+                    .buildAndExpand(pilot.get().getId(), licenseToAdd.getId()).toUri()).build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PutMapping("{licenseId}")
+    public ResponseEntity<Void> updateLicense(@PathVariable ("pilotId") int pilotId,
+                                              @RequestBody UpdateLicenseRequest request,
+                                              @PathVariable("licenseId") int licenseId){
+        Optional<License> licenseToUpdate = licenseService.find(pilotId, licenseId);
+        if(licenseToUpdate.isPresent()){
+            UpdateLicenseRequest.dtoToEntityUpdater().apply(licenseToUpdate.get(), request);
+            licenseService.update(licenseToUpdate.get());
+            return ResponseEntity.accepted().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteLicense(@PathVariable("pilotId") int pilotId)
 }
