477094ef98c5f9b0136dd1c8365f6355b584dbff
Pestap <90579181+Pestap@users.noreply.github.com>
2022-10-20 11:43:24 +0200

    GetPilotsResponse uses static class to create repsonse

--- src/main/java/eti/pg/lab/pilot/dto/GetPilotsResponse.java
+++ src/main/java/eti/pg/lab/pilot/dto/GetPilotsResponse.java
@@ -3,6 +3,7 @@ package eti.pg.lab.pilot.dto;
 import eti.pg.lab.pilot.entity.Pilot;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
@@ -16,15 +17,34 @@ import java.util.function.Function;
 @EqualsAndHashCode
 public class GetPilotsResponse {
 
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @ToString
+    @EqualsAndHashCode
+    public static class PilotEntry{
+        private String name;
+        private String surname;
+        private LocalDate dateOfBirth;
+        private int numberOfLicenses;
+    }
     @Singular
-    private List<String> pilots;
+    private List<PilotEntry> pilots;
 
     public static Function<Collection<Pilot>, GetPilotsResponse> entityToDtoMapper(){
         return pilots -> {
                 GetPilotsResponseBuilder response = GetPilotsResponse.builder();
 
                 pilots.stream()
-                        .map(pilot -> pilot.getName() + " " + pilot.getSurname() + ", id: " + pilot.getId()+", licenses: " + pilot.getLicenseList().size())
+                        .map(pilot -> PilotEntry.builder()
+                                .name(pilot.getName())
+                                .surname(pilot.getSurname())
+                                .dateOfBirth(pilot.getDateOfBirth())
+                                .numberOfLicenses(pilot.getLicenseList().size())
+                                .build())
                         .forEach(response::pilot);
                 return response.build();
         };
